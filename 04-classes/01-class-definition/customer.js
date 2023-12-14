14f0b40d8c143f8d3247fb2bd271458638445deb
ashutosh7131 <ashutosh713@outlook.com>
2023-12-15 01:11:41 +0800

    class commit

---
+++ 04-classes/01-class-definition/customer.js
@@ -0,0 +1,10 @@
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    return Customer;
+}());
+var myCustomer = new Customer();
+myCustomer.firstName = "Ashutosh";
+myCustomer.lastName = "Shriansh";
+console.log("FirstName is :   " + myCustomer.firstName);
+console.log("LastName is :   " + myCustomer.lastName);
---
+++ 04-classes/01-class-definition/customer.ts
@@ -0,0 +1,11 @@
+class Customer{
+
+    firstName: String;
+    lastName: String;
+}
+
+let myCustomer = new Customer();
+myCustomer.firstName = "Ashutosh";
+myCustomer.lastName = "Shriansh";
+console.log("FirstName is :   " + myCustomer.firstName);
+console.log("LastName is :   " + myCustomer.lastName);
