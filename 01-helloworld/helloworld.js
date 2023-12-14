6fa80241aff38df054d33a13523fb989c3c7efb7
ashutosh7131 <ashutosh713@outlook.com>
2023-12-14 18:40:17 +0800

    second commit

--- 01-helloworld/helloworld.js
+++ 01-helloworld/helloworld.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+var found = true;
+var grade = 99;
+var firstName = "Ashutosh";
+var lastName = "Shriansh";
+console.log(found);
+console.log("The Grade is : " + grade);
+console.log("Hi ::  " + firstName + "   " + lastName);
+console.log("Hi :: Without concatenation \" ".concat(firstName, " ").concat(lastName));
