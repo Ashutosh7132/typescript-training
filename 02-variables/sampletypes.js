37478bcc2df060db35316956fd035d3771583561
ashutosh7131 <ashutosh713@outlook.com>
2023-12-14 18:46:29 +0800

    3rd commit

---
+++ 02-variables/sampletypes.js
@@ -0,0 +1,6 @@
+var grade = 99;
+var firstName = "Ashutosh";
+var lastName = "Shriansh";
+console.log("The Grade is : " + grade);
+console.log("Hi ::  " + firstName + "   " + lastName);
+console.log("Hi :: Without concatenation \" ".concat(firstName, " ").concat(lastName));
---
+++ 02-variables/sampletypes.ts
@@ -0,0 +1,6 @@
+let grade: number = 99;
+let firstName: String = "Ashutosh";
+let lastName: String = "Shriansh";
+console.log("The Grade is : " + grade);
+console.log("Hi ::  " + firstName + "   " + lastName);
+console.log(`Hi :: Without concatenation " ${firstName} ${lastName}`);
